a9a528143a0984b09c3a7895fed9af794fec63a6
Ola Okraska <okraskaola@gmail.com>
2020-04-19 11:16:16 +0200

    add function to clear screen (closes #7)

--- game/game.py
+++ game/game.py
@@ -1,6 +1,13 @@
 from random import randint
 from operator import attrgetter
 
+# import only system from os
+from os import system, name
+
+# import sleep to show output for some time period
+from time import sleep
+
+
 CARD_TEMPLATE = """"
          ┌─────────┐
          │{}        │
@@ -15,6 +22,7 @@ CARD_TEMPLATE = """"
 
 class Karta:
     """Jeden z dziewięciu elementów."""
+    czy_odkryta = False
 
     def __init__(self, imie):
         self.imie = imie
@@ -23,6 +31,7 @@ class Karta:
         first_letter = self.imie[0]
         card = CARD_TEMPLATE.format(first_letter, self.imie, first_letter)
         return card
+
 
 KARTY = [Karta("Dżaga"), Karta("Gandzia"), Karta("Zadra"),
          Karta("Ciri"), Karta("Bazia"), Karta("Florka"),
@@ -36,17 +45,12 @@ def wyswietl_karty(karty):
             line = ""
             for card in range(3):
                 numer_karty = 3 * card_row + card
-                card_line_str = str(KARTY[numer_karty])
+                card_line_str = str(karty[numer_karty])
                 card_lines = card_line_str.split('\n')
                 line += card_lines[card_line]
             lines.append(line)
             print(line)
     return str(lines)
-
-
-def czysc_ekran():
-    # czyszczenie ekranu
-    pass
 
 
 def mieszaj_karty(karty):
@@ -57,10 +61,30 @@ def mieszaj_karty(karty):
     return karty
 
 
+def clear():
+
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
 
 def gra():
     """Funkcja przeprowadzająca grę."""
     karty = mieszaj_karty(KARTY)
+    wyswietl_karty(karty)
+
+    # sleep for 2 seconds after printing output
+    sleep(5)
+
+    # now call function we defined above
+    clear()
+
+    sleep(5)
+
     wyswietl_karty(karty)
 
 
